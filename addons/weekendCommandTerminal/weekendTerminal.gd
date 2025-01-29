d73256c8ff8ee82b6868c8fa349d1aff609c25c0
ben <ben@no-weekend.com>
2025-01-30 09:51:15 +1100

    Fixed some terminal bugs

--- addons/weekendCommandTerminal/weekendTerminal.gd
+++ addons/weekendCommandTerminal/weekendTerminal.gd
@@ -1,8 +1,9 @@
-extends Node
+extends CanvasLayer
 
 @onready var COMMAND_TERMINAL : PackedScene = preload("res://addons/weekendCommandTerminal/command_terminal.tscn")
 
 func _ready() -> void:
+	layer = 100
 	var terminal = COMMAND_TERMINAL.instantiate()
 	add_child(terminal)
 	#terminal._ready()
